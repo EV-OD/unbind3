ab5ba9cb892b617108be6578495e4925ee2766eb
Atul0002 <1atultiwari062@gmail.com>
2023-12-18 23:06:05 +0545

    deleted console.log

--- src/store.js
+++ src/store.js
@@ -4,14 +4,13 @@ export async function initializeStore(initialState) {
   const { feature } = await chrome.storage.sync.get(["feature"]);
   if (!feature) {
     setFeature(initialState, () => {
-      console.log("done");
+      
     });
     notifyOthers();
   }
 }
 
 export function setFeature(obj, func) {
-  console.log(obj);
   chrome.storage.sync.set({ feature: obj }).then(() => {
     func();
   });

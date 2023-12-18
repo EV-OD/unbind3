8c320697cb3fe031c7e5f011c602c1328f2a0a33
EV-OD <evod599@gmail.com>
2023-12-18 23:02:10 +0545

    bug fix

--- src/utils/redirectToSubscription.js
+++ src/utils/redirectToSubscription.js
@@ -1,14 +1,48 @@
+// export default function redirectToSubscription(feature) {
+//   let website = window.location.href;
+//   if (feature["youtube-redirect-to-subscription"]) {
+//     if (
+//       feature["youtube-redirect-to-subscription"].enabled &&
+//       website.includes(feature["youtube-redirect-to-subscription"].website)
+//     ) {
+//       if (window.location.href === "https://www.youtube.com/") {
+//         var subscriptionURL =
+//           window.location.protocol +
+//           "//" +
+//           window.location.host +
+//           "/feed/subscriptions";
+//         window.location.replace(subscriptionURL);
+//       }
+//     }
+//   }
+// }
+
 export default function redirectToSubscription(feature) {
-    let website = window.location.href;
-    if (feature["youtube-redirect-to-subscription"]) {
-      if (
-        feature["youtube-redirect-to-subscription"].enabled &&
-        website.includes(feature["youtube-redirect-to-subscription"].website)
-      ){
-        if(window.location.href === "https://www.youtube.com/"){
-            var subscriptionURL = window.location.protocol + "//" +  window.location.host +  "/feed/subscriptions";
-            window.location.replace(subscriptionURL);
-        }
-        }
+  let website = window.location.href;
+
+  if (feature["youtube-redirect-to-subscription"]) {
+    if (
+      feature["youtube-redirect-to-subscription"].enabled &&
+      website.includes(feature["youtube-redirect-to-subscription"].website)
+    ) {
+      var currentURL = window.location.href;
+      if (currentURL == "https://www.youtube.com/") {
+        var subscriptionURL =
+          window.location.protocol +
+          "//" +
+          window.location.host +
+          "/feed/subscriptions";
+        window.location.replace(subscriptionURL);
+      }
+      //   if (!/\/subscriptions$/.test(currentURL)) {
+      //     var subscriptionURL =
+      //       window.location.protocol +
+      //       "//" +
+      //       window.location.host +
+      //       "/feed/subscriptions";
+
+      //     window.location.replace(subscriptionURL);
+      //   }
     }
+  }
 }
